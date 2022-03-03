b46c73687ece0c16d4cb0309d0db289edb91e177
Roberto M. Diniz <78979759+robertomdiniz5@users.noreply.github.com>
2022-03-03 16:11:47 -0300

    Create apt_favorites.sh

---
+++ apt_favorites.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y vim 
+sudo apt-get install -y tilix
+sudo apt-get install -y cmatrix
+sudo apt-get install -y goodvibes
+sudo apt-get install -y gnome-tweaks
+sudo apt-get install -y gnome-shell-pomodoro
