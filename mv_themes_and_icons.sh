921dcbba015769dcc3e77e295f877126fc4c264c
Roberto M. Diniz <78979759+robertomdiniz5@users.noreply.github.com>
2022-03-03 15:24:13 -0300

    Update and rename THEMES_AND_ICONS to mv_themes_and_icons.sh

---
+++ mv_themes_and_icons.sh
@@ -0,0 +1,9 @@
+cp all_icons_2202.zip /usr/share/icons/
+cp all_themes_2202.zip /usr/share/themes/
+cd /usr/share/icons/
+unzip all_icons_2202.zip
+cd /usr/share/themes/
+unzip all_themes_2202.zip
+
+#THEMES_AND_ICONS
+#https://drive.google.com/drive/folders/1-oCW-7x66gqGalHerdk4j0WvWOlfNnOl?usp=sharing
