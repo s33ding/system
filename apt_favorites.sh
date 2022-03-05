2ada8670b4001633c82dd75218c7bb4283473383
robertomdiniz5 <robertomdiniz@protonmail.com>
2022-03-05 20:18:03 -0300

    ok

--- apt_favorites.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install -y vim 
-sudo apt-get install -y tilix
-sudo apt-get install -y cmatrix
-sudo apt-get install -y goodvibes
-sudo apt-get install -y gnome-tweaks
-sudo apt-get install -y gnome-shell-pomodoro
