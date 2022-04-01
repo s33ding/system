724a3f0fc6057796474389056f5b2f9f14dc66c0
robertomdiniz5 <robertomdiniz@protonmail.com>
2022-04-01 16:36:55 -0300

    ok

--- install_apts_flatpaks.sh
+++ install_apts_flatpaks.sh
@@ -1,9 +1,9 @@
-#file=packages_apts.txt
-#for packages in $(cat $file); do 
-#    sudo apt-get install $packages -y; 
-#done
+file=packages_apts.txt
+for packages in $(cat $file); do 
+   sudo apt-get install $packages -y; 
+done
 
-#file=packages_flatpaks.txt
-#for packages in $(cat $file); do
-#    echo $packages -y;
-#done
+file=packages_flatpaks.txt
+for packages in $(cat $file); do
+   flatpak install flathub $packages -y;
+done
--- tor-browser.sh
+++
@@ -1,1 +0,0 @@
-wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux64-11.0.6_en-US.tar.xz
