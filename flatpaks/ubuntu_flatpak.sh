9ced53680b094f63cf182b5c5e78ea269c35bb03
s33ding <robertomdiniz@protonmail.com>
2022-05-24 18:29:58 -0300

    ok

---
+++ flatpaks/ubuntu_flatpak.sh
@@ -0,0 +1,7 @@
+sudo apt install flatpak
+
+sudo add-apt-repository ppa:flatpak/stable
+sudo apt update
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
